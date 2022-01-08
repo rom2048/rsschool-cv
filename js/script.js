30040deab88d01d0712a2bd9033baef3b323f9f5
Roman <mamontov.r1995@gmail.com>
2022-01-08 23:59:08 +0300

    feat: update cv

--- js/script.js
+++ js/script.js
@@ -8,3 +8,31 @@ if (iconMenu) {
         menuBody.classList.toggle('_active');
     })
 }
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]')
+if (menuLinks) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick);
+    });
+
+    function onMenuLinkClick(e){
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
+
+            if (iconMenu.classList.contains('_active')){
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            });
+        }
+
+        e.preventDefault();
+    }
+}
